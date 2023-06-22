3291b47b76b8e71e45ef3522901644fb1054db38
DolphyWind <yunusaydin590@gmail.com>
2023-06-22 23:23:50 +0300

    Delete SFEX and add CLIP, FMT

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include <TGUI/TGUI.hpp>
 #include <iostream>
+#include <box2d/box2d.h>
+#include <fmt/format.h>
+#include <clip.h>
 
 int main()
 {
+    b2Vec2 gravity(0.0f, -9.81f);
+    fmt::print("Hello world! {}", gravity.y);
+
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     tgui::GuiSFML gui(window);
     tgui::Button::Ptr testButton = tgui::Button::create("Click Me!");
