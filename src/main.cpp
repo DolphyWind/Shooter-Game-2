f193ced85edf83357d654d0cb1c51a841f9db601
DolphyWind <yunusaydin590@gmail.com>
2023-06-24 17:47:41 +0300

    SFML CMake Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,70 @@
 #include <SFML/Graphics.hpp>
-#include <TGUI/TGUI.hpp>
 #include <iostream>
-#include <box2d/box2d.h>
-#include <fmt/format.h>
 
 int main()
 {
-    b2Vec2 gravity(0.0f, -9.81f);
-    fmt::print("Hello world! {}", gravity.y);
+    // b2Vec2 gravity(0.0f, -9.81f);
+    // fmt::print("Hello world! {}", gravity.y);
+
+    // sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    // tgui::GuiSFML gui(window);
+    // tgui::Button::Ptr testButton = tgui::Button::create("Click Me!");
+
+    // bool pressed = false;
+    // testButton->onPress([&](){
+    //     if(pressed) return;
+    //     tgui::MessageBox::Ptr messageBox = tgui::MessageBox::create("Thanks!", "You've pressed me!", {"Ok", "Close"});
+    //     messageBox->setWidgetName("msgBox");
+    //     messageBox->onButtonPress([&](tgui::String buttonStr){
+    //         if(buttonStr == "Ok")
+    //         {
+    //             gui.remove(gui.get("msgBox"));
+    //             pressed = false;
+    //         }
+    //         else if(buttonStr == "Close")
+    //         {
+    //             window.close();
+    //         }
+    //     });
+
+    //     gui.add(messageBox);
+    //     pressed = true;
+    // });
+    // gui.add(testButton);
+
+    // while (window.isOpen())
+    // {
+    //     sf::Event event{};
+    //     while (window.pollEvent(event))
+    //     {
+    //         gui.handleEvent(event);
+    //         if (event.type == sf::Event::Closed)
+    //             window.close();
+    //     }
+
+    //     window.clear();
+
+    //     gui.draw();
+    //     window.display();
+    // }
 
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    tgui::GuiSFML gui(window);
-    tgui::Button::Ptr testButton = tgui::Button::create("Click Me!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    bool pressed = false;
-    testButton->onPress([&](){
-        if(pressed) return;
-        tgui::MessageBox::Ptr messageBox = tgui::MessageBox::create("Thanks!", "You've pressed me!", {"Ok", "Close"});
-        messageBox->setWidgetName("msgBox");
-        messageBox->onButtonPress([&](tgui::String buttonStr){
-            if(buttonStr == "Ok")
-            {
-                gui.remove(gui.get("msgBox"));
-                pressed = false;
-            }
-            else if(buttonStr == "Close")
-            {
-                window.close();
-            }
-        });
-
-        gui.add(messageBox);
-        pressed = true;
-    });
-    gui.add(testButton);
+    std::cout << SFML_VERSION_MAJOR << '.' << SFML_VERSION_MINOR << '.' << SFML_VERSION_PATCH << std::endl;
 
     while (window.isOpen())
     {
-        sf::Event event{};
+        sf::Event event;
         while (window.pollEvent(event))
         {
-            gui.handleEvent(event);
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         window.clear();
-
-        gui.draw();
+        window.draw(shape);
         window.display();
     }
 
